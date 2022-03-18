687b9d17f27d3d21c606e6b0f8b0ff7e43b0b69a
Naufal Taufiq Ridwan <71002775+n0l3r@users.noreply.github.com>
2022-03-18 18:12:59 +0700

    Update handson1_6_120140044.sh

--- handson1_6_120140044.sh
+++ handson1_6_120140044.sh
@@ -1,7 +1,7 @@
 # Cek apakah script ini dijalankan dengan argument
 if [ $# -eq 0 ]; then
     echo "Tidak ada argument yang diberikan." # Jika tidak ada argument, maka tampilkan pesan ini
-    exit 0
+    exit 0001
 fi
 
 # Cek apakah file ada di directory atau tidak
